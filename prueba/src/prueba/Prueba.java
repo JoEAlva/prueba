a978c1f2400c845ae90de2e257d90c39b2b319b4
JorgeIgnacio <JorgeIgnacio@Jupiter>
2016-06-24 00:28:55 -0600

    programación por la noche

---
+++ prueba/src/prueba/Prueba.java
@@ -0,0 +1,19 @@
+/*
+ * @Autor Jorge Ignacio Elizondo Alvarado
+ */
+package prueba;
+
+/**
+ *
+ * @author JorgeIgnacio
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
